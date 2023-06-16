7839e266bcce90223029978ad3455be1277b846b
jiahnkim230502 <locker9417@gmail.com>
2023-06-16 10:17:44 +0900

    두번째 커밋

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const Comments = require("../schemas/comment.js");
 
 router.get("/comments/:postid", async (req, res) => {
     var { postid } = req.params
-    const allComments = await Comments.find({"postid":postid});
+    const allComments = await Comments.find({ "postid": postid });
     allComments.sort(
         function (prev, next) {
             if (prev.date > next.date) { return -1 }
@@ -15,48 +15,49 @@ router.get("/comments/:postid", async (req, res) => {
 
     if (!allComments.length) {
         return res.status(200).json({
-            "message": "작성된 댓글이 없습니다. 첫 작성자가 되어 주세요!"
+            "message": "댓글 없음"
         })
     } else {
-    return res.status(200).json({"posts": allComments})}
+        return res.status(200).json({ "posts": allComments })
+    }
 });
 
 
 router.post("/comments/:postid", async (req, res) => {
     var { postid } = req.params
-    var {user, password, content} = req.body;
+    var { user, password, content } = req.body;
     var date = new Date()
     if (!content) {
         return res.status(400).json({
             success: false,
-            errorMessage: "작성할 댓글 내용을 입력해주세요."
+            errorMessage: "댓글을 작성해 주세요."
         })
     } else {
-        await Comments.create({user, password, content, date, postid})
-        return res.status(201).json({"message": "댓글이 작성되었습니다."})
+        await Comments.create({ user, password, content, date, postid })
+        return res.status(201).json({ "message": "댓글이 작성되었습니다." })
     }
 })
 
-router.put("/comments/:postid", async(req, res) => {
-    var {postid} = req.params;
-    var {user, password, content} = req.body;
+router.put("/comments/:postid", async (req, res) => {
+    var { postid } = req.params;
+    var { user, password, content } = req.body;
     var date = new Date();
 
-    
+
     if (!content) {
         return res.status(400).json({
             success: false,
             errorMessage: "수정할 댓글 내용을 입력해주세요."
-        })}
-        else{
-            await Comments.updateOne({ user, password, content, date, postid })
+        })
+    } else {
+        await Comments.updateOne({ user, password, content, date, postid })
         return res.status(201).json({ "message": "댓글이 수정되었습니다." })
-        }   
+    }
 })
 
-router.delete("/comments/:postid", async(req, res) => {
-    var {postid} = req.params;
-    var {user, password} = req.body;
+router.delete("/comments/:postid", async (req, res) => {
+    var { postid } = req.params;
+    var { user, password } = req.body;
     if (Comments.find({ "postid": postid, "user": user, "password": password })) {
         await Comments.deleteOne({ "user": user, "password": password })
         return res.status(200).json({
--- routes/posts.js
+++ routes/posts.js
@@ -17,63 +17,63 @@ router.get("/posts", async (req, res) => {
             "message": "게시글 없음"
         })
     } else {
-    return res.status(200).json({"posts": allPosts})
-}
+        return res.status(200).json({ "posts": allPosts })
+    }
 });
 
 // 게시글 1개 가져오기
 router.get("/posts/:postid", async (req, res) => {
     const { postid } = req.params
-    const postfind = await Posts.findOne({"_id": postid});
-    if(postfind){
+    const postfind = await Posts.findOne({ "_id": postid });
+    if (postfind) {
         return res.status(200).json(
-            {"선택":postfind}
+            { "선택": postfind }
         )
     }
 
-   
+
 });
 
 router.post("/posts", async (req, res) => {
-    var {user, password, title, content} = req.body;
+    var { user, password, title, content } = req.body;
 
     if (user, password, title, content) {
         var date = new Date()
 
-        await Posts.create({user, password, title, content, date})
-        return res.status(201).json({"message": "게시글이 작성되었습니다."})
+        await Posts.create({ user, password, title, content, date })
+        return res.status(201).json({ "message": "게시글이 작성되었습니다." })
     }
 })
 
-router.put("/posts/:postid", async(req, res) => {
-    var {postid} = req.params;
-    var {user, password, title, content} = req.body;
-    const existPost = await Posts.find({"_id": postid, "password": password});
+router.put("/posts/:postid", async (req, res) => {
+    var { postid } = req.params;
+    var { user, password, title, content } = req.body;
+    const existPost = await Posts.find({ "_id": postid, "password": password });
     if (existPost.length) {
         var date = new Date();
-        await Posts.updateOne({user, password, title, content, date});
+        await Posts.updateOne({ user, password, title, content, date });
         return res.status(200).json({
-            "message" : "수정 완료!"
+            "message": "수정 완료!"
         })
     } else {
         return res.status(400).json({
-        "error message" : "비밀번호 불일치!"
+            "error message": "비밀번호 불일치!"
         });
     }
 })
 
-router.delete("/posts/:postid", async(req, res) => {
-    var {postid} = req.params;
-    var {password} = req.body;
-    const existPost = await Posts.find({"_id": postid, "password":password});
+router.delete("/posts/:postid", async (req, res) => {
+    var { postid } = req.params;
+    var { password } = req.body;
+    const existPost = await Posts.find({ "_id": postid, "password": password });
     if (existPost.length) {
-        await Posts.deleteOne({"_id": postid, "password":password});
+        await Posts.deleteOne({ "_id": postid, "password": password });
         return res.status(200).json({
             "message": "삭제 완료!"
         })
     } else {
         return res.status(400).json({
-            "error message" : "비밀번호 불일치!"
+            "error message": "비밀번호 불일치!"
         })
     }
 })
